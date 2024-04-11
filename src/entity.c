91e0635749d13faae2558de118794023c235bc7f
Flip <flip@pop-os.localdomain>
2024-04-11 16:24:04 +0200

    Added rudimentary foreshadow

--- src/entity.c
+++ src/entity.c
@@ -44,11 +44,27 @@ void	move_entities(t_entity *ent, t_app *cbd)
 	}
 }
 
+void	update_foreshadowing(t_entity *ent, float dt)
+{
+	t_vec2d		new_pos;
+
+	new_pos = ent->pos;
+	ent->dir = vec_assign(-1.0, 0.0);
+	new_pos = vec_add(new_pos, vec_mult(ent->dir, ent->speed * dt));
+	ent->pos = new_pos;
+}
+
 void	update_enemy(t_entity *ent, t_app *cbd)
 {
 	t_audio *audio;
 
 	audio = cbd->audio;
+	if (vec_distance(ent->pos, cbd->playerdata.pos) < 3.5 && ft_strncmp("trigger2", ent->name, 8) == 0)
+	{
+		update_foreshadowing(ent, cbd->mlx->delta_time);
+		if ((int) ent->pos.x < 21)
+			ent->enabled = false;
+	}
 	if (vec_distance(ent->pos, ent->destinations[ent->current_dest]) < 0.1)
 	{
 		ent->state = ENTITY_IDLE;
